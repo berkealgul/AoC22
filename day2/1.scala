1b95ac48a0abe5d94a5cbdf37581b01a7c3cb354
berkealgul <berkealgul@gmail.com>
2023-01-24 22:12:45 +0300

    init

---
+++ day2/1.scala
@@ -0,0 +1,15 @@
+import scala.io.Source
+
+
+object Main {
+    def main(args: Array[String]) = {
+        var filename = "../inputs/day2/input1.txt"
+
+        var R
+
+        for (line <- Source.fromFile(filename).getLines) {
+            
+
+        }
+    }
+}
