891050bc6dfad27f24eeefb17d0add481ecd217d
berkealgul <berkealgul@gmail.com>
2023-02-06 06:02:33 +0300

    wip2

--- day5/1.scala
+++ day5/1.scala
@@ -5,28 +5,40 @@ import util.control.Breaks._
 object Main {
 
     def main(args: Array[String]) = {
-        var filename = "../inputs/input4.txt"
+        var filename = "../inputs/input5-adjusted.txt"
 
-        // define array of stacks
+        var stacks: Array[Stack[Char]] = Array(
+            Stack('F', 'G', 'V', 'R', 'J', 'L', 'D'),
+            Stack('S', 'J', 'H', 'V', 'B', 'M', 'P', 'T'),
+            Stack('P'))
 
-        var stacks = Array[Stack[Char]] = new Array[Stack[Char]](3)
-        stacks(0) = new Stack[Char]('Z', 'N')
-        stacks(1) = new Stack[Char]('M', 'C', 'D')
-        stacks(2) = new Stack[Char]('P')   
+        var tops = ""
 
         for (line <- Source.fromFile(filename).getLines) {
+            if(line.trim != "") {
+                var args = line.split(" ")
             
-            var args = line.split(" ")
+                var amount = args(1).toInt
+                var src = args(3).toInt - 1
+                var des = args(5).toInt - 1 
 
-            var amount = args(1).toInt
-            var src = args(3).toInt
-            var des = args(5).toInt
+
+                for(stack <- stacks) {
+                    println(stack)
+                }
+                println("---")
+
+                for(i <- 0 until amount) {
+                    var top = stacks(src).pop()
+                    stacks(des).push(top)
+                }
+            }
         }
-    
+
+        for(stack <- stacks) {
+            tops += stack.top
+        }
+
+        println(tops)
     }
 }
-
-//    [D]    
-//[N] [C]    
-//[Z] [M] [P]
-// 1   2   3 
