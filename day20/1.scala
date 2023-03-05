8b14c8d9bb3c82d2d2d30c06e7b413ca7867d137
berkealgul <berkealgul@gmail.com>
2023-03-05 07:07:59 +0300

    20.1 wip

--- day20/1.scala
+++ day20/1.scala
@@ -1,20 +1,65 @@
 import scala.io.Source
-import scala.collection.mutable.Map
+import scala.collection.mutable.Queue
+import scala.collection.mutable.ArrayBuffer
 import util.control.Breaks._
 
-object Node {
-    var v = 0;
-    Node next;
+class Node {
+    var v: Int = 0
+    var i: Int = 0
+
 }
 
 object Main {
     def main(args: Array[String]) = {
         var filename = "../inputs/testcase.txt"
 
+        var stack: Queue[Node] = Queue()
+        var array: ArrayBuffer[Node] = ArrayBuffer()
+        var i = 0
+
         for(line <- Source.fromFile(filename).getLines) {
-            if(line)         
+            if(line.trim != "") {
+                var n: Node = Node();
+                n.v = line.toInt
+                n.i = i
+                i += 1
+
+                stack += n
+                array += n
+            }
+        }
+
+        while(stack.length > 0) {
+            for(nx <- array) {
+                print(nx.v)
+                print(" ")
+            }
+
+            println()
+
+            var n: Node = stack.dequeue()
+            var id = n.i
+            var v = n.v
+
+            //println(v)
+
+            var j = id + v
+            j = ((j % array.length) + array.length) % array.length
+
+            var nt: Node = array(j)
+            array(j) = n
+            array(id) = nt
+            n.i = j
+            nt.i = id
+
 
         }
 
+        for(nx <- array) {
+            print(nx.v)
+            print(" ")
+        }
+
+        println()
     }
 }
