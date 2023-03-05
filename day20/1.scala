8d8df2414de575aa8be744b1e1c92674469cfc44
berkealgul <berkealgul@gmail.com>
2023-03-05 06:18:25 +0300

    day7- day20 init

---
+++ day20/1.scala
@@ -0,0 +1,20 @@
+import scala.io.Source
+import scala.collection.mutable.Map
+import util.control.Breaks._
+
+object Node {
+    var v = 0;
+    Node next;
+}
+
+object Main {
+    def main(args: Array[String]) = {
+        var filename = "../inputs/testcase.txt"
+
+        for(line <- Source.fromFile(filename).getLines) {
+            if(line)         
+
+        }
+
+    }
+}
---
+++ day7/1.scala
@@ -0,0 +1,15 @@
+import scala.io.Source
+import scala.collection.mutable.Map
+import util.control.Breaks._
+
+object Main {
+    def main(args: Array[String]) = {
+        var filename = "../inputs/testcase.txt"
+
+        for(line <- Source.fromFile(filename).getLines) {
+                        
+
+        }
+
+    }
+}
